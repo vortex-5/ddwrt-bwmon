300ef30bdd8359a4935e8b8321545fa81d03554f
Vortex-5 <vortex-5@users.noreply.github.com>
2014-12-18 00:19:20 -0500

    Added mac address display

--- bwmon/www/mac-names.js
+++ bwmon/www/mac-names.js
@@ -1,8 +1,9 @@
 /**
  * This file is used by the HTML tool to match up friendly names please follow the format below (this is a standard javascript dictionary).
+ * Mac addresses must be in lowercase here.
  */
 
  var MAC_NAMES = {
-'01:23:45:67:89:AB': 'First and every other pc name',
+'01:23:45:67:89:ab': 'First and every other pc name',
 '00:11:22:33:44:55': 'Last PC Name'
 };
