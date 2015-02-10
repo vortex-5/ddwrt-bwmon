5cda2761cb0ff81c464bb4e393b82730afa97950
vortex-5 <vortex-5@users.noreply.github.com>
2015-02-10 03:02:47 -0500

    Extended delay to 30 seconds to try and avoid non configured dns issue and as a result no mac names.

--- bwmon/startup.sh
+++ bwmon/startup.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(cd ${SCRIPT_DIR} && pwd)
 $SCRIPT_DIR/stop.sh
 
 # Give the router some time to get the dnsmasq configuration up
-sleep 10
+sleep 30
 
 # Start new session
 $SCRIPT_DIR/start.sh
