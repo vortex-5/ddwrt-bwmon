59468114ad0d2e08c90a9dea3ac601fc3939c21c
Vortex-5 <vortex-5@users.noreply.github.com>
2015-02-09 20:18:15 -0500

    Fixed an issue with the startup script when it loads faster than the dnsmasq

--- bwmon/startup.sh
+++ bwmon/startup.sh
@@ -5,5 +5,8 @@ SCRIPT_DIR=$(cd ${SCRIPT_DIR} && pwd)
 # Terminate pervious session
 $SCRIPT_DIR/stop.sh
 
+# Give the router some time to get the dnsmasq configuration up
+sleep 10
+
 # Start new session
 $SCRIPT_DIR/start.sh
