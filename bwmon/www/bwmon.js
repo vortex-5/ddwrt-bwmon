b7d433b699bdbbf3a090444138c8a8855b61d9e3
Vortex-5 <vortex-5@users.noreply.github.com>
2015-02-14 12:16:25 -0500

    Fixed issue with mac addresses being the wrong case

--- bwmon/www/bwmon.js
+++ bwmon/www/bwmon.js
@@ -1,13 +1,17 @@
+'use strict'
+
 var bwmon = angular.module('bwmonApp', []);
 
 bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
+	
 	$scope.SCRIPT_INTERVAL = 10;
 	$scope.CONVERSION_FACTOR = 8/$scope.SCRIPT_INTERVAL; // From KB/s to Kbps
 	$scope.POLL_WAIT_TIME = $scope.SCRIPT_INTERVAL;
-  $scope.usageData = [];
+	$scope.usageData = [];
 	$scope.pollCountDown = 0;
+	$scope.macNames = {};
 
-  $scope.init = function() {	
+	$scope.init = function() {
 		(function tick() {
 			if ($scope.pollCountDown > 1) {
 				$scope.pollCountDown--;
@@ -21,83 +25,90 @@ bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($sco
 			}
 			$timeout(tick, 1000);
 		})();
-  };
 
-  $scope.updateUsageData = function(data) {
-    var resultLines = data.split('\n');
+		var macNamesFile = MAC_NAMES; // Required mac names won't read if it's not in a var.
+		for (var mac in macNamesFile) {
+			if (macNamesFile.hasOwnProperty(mac)) {
+				$scope.macNames[mac.toUpperCase()] = MAC_NAMES[mac];
+			}
+		}
+	};
 
-    angular.forEach(resultLines, function(line) {
-    
-      if (line.trim().length > 0) {
-        var entry = line.split(',');        
-        var item = {};
-        
-        item.mac = entry[0];
-        item.postDown = Number(entry[1]);
-        item.postUp = Number(entry[2]);
-        item.preDown = Number(entry[3]);
-        item.preUp = Number(entry[4]);
-        item.date = entry[5].replace(' ', ' ');
-        
-        $scope.usageData.push(item);
-      }
-    });
-  };
-  
-  $scope.getName = function(macAddress) {
-    var name = MAC_NAMES[macAddress];
-    return name ? name : macAddress
-  };
-  
-  $scope.round = function(value) {
-    return Math.round(value * 100)/100;
-  };
-  
-  $scope.getSize = function(KB) {  
-    if (KB / Math.pow(1024, 2) > 1)
-      return $scope.round(KB/Math.pow(1024, 2)) + ' GB';
+	$scope.updateUsageData = function(data) {
+		var resultLines = data.split('\n');
 
-    if (KB / 1024 > 1)
-      return $scope.round(KB/1024) + ' MB';
-      
-    return $scope.round(KB) + ' KB';
-  };
-  
-  $scope.getRate = function(Kbps) {    
-    if (Kbps / 1000 > 1)
-      return $scope.round(Kbps/1000) + ' Mbps';
-      
-    return $scope.round(Kbps) + ' Kbps';
-  };
+		angular.forEach(resultLines, function(line) {
 
-	$scope.getDeviceTotal = function(device) {			
+			if (line.trim().length > 0) {
+				var entry = line.split(',');
+				var item = {};
+
+				item.mac = entry[0];
+				item.postDown = Number(entry[1]);
+				item.postUp = Number(entry[2]);
+				item.preDown = Number(entry[3]);
+				item.preUp = Number(entry[4]);
+				item.date = entry[5].replace(' ', ' ');
+
+				$scope.usageData.push(item);
+			}
+		});
+	};
+
+	$scope.getName = function(macAddress) {
+		var name = $scope.macNames[macAddress.toUpperCase()];
+		return name ? name : macAddress.toUpperCase()
+	};
+
+	$scope.round = function(value) {
+		return Math.round(value * 100)/100;
+	};
+
+	$scope.getSize = function(KB) {
+		if (KB / Math.pow(1024, 2) > 1)
+			return $scope.round(KB/Math.pow(1024, 2)) + ' GB';
+
+		if (KB / 1024 > 1)
+			return $scope.round(KB/1024) + ' MB';
+
+		return $scope.round(KB) + ' KB';
+	};
+
+	$scope.getRate = function(Kbps) {
+		if (Kbps / 1000 > 1)
+			return $scope.round(Kbps/1000) + ' Mbps';
+
+		return $scope.round(Kbps) + ' Kbps';
+	};
+
+	$scope.getDeviceTotal = function(device) {
 		if (!device)
 			return 0;
-	
+
 		return Number(device.postUp) + Number(device.postDown);
 	};
-	
+
 	$scope.getDeviceTotalRate = function(device) {
 		if (!device)
 				return 0;
-				
+
 		return $scope.getDownRate(device) + $scope.getUpRate(device);
 	};
-	
+
 	$scope.getDownRate = function(device) {
 		if (!device)
 			return 0;
-			
+
 		return (device.postDown - device.preDown) * $scope.CONVERSION_FACTOR;
 	};
-	
+
 	$scope.getUpRate = function(device) {
 		if (!device)
 			return 0;
-			
+
 		return (device.postUp - device.preUp) * $scope.CONVERSION_FACTOR;
 	};
-	
+
 	$scope.getTotals = function(devices) {
 		var subTotal = 0;
 		angular.forEach(devices, function(device) {
@@ -105,7 +116,7 @@ bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($sco
 		});
 		return subTotal;
 	}
-	
+
 	$scope.getTotalDown = function(devices) {
 		var total =  0;
 		angular.forEach(devices, function(device) {
@@ -113,7 +124,7 @@ bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($sco
 		});
 		return total;
 	};
-	
+
 	$scope.getTotalUp = function(devices) {
 		var total =  0;
 		angular.forEach(devices, function(device) {
@@ -121,7 +132,7 @@ bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($sco
 		});
 		return total;
 	};
-	
+
 	$scope.getTotalDownRate = function(devices) {
 		var total =  0;
 		angular.forEach(devices, function(device) {
@@ -129,7 +140,7 @@ bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($sco
 		});
 		return total;
 	};
-	
+
 	$scope.getTotalUpRate = function(devices) {
 		var total =  0;
 		angular.forEach(devices, function(device) {
@@ -137,11 +148,11 @@ bwmon.controller('MainController', ['$scope', '$timeout', '$http', function($sco
 		});
 		return total;
 	};
-	
+
 	$scope.sortFunction = function(device) {
 		var total = device.postUp + device.postDown;
 		return total;
 	};
 
-  $scope.init();
+	$scope.init();
 }]);
