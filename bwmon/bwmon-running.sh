ebc391e1772991e53516389b36d7d6b51a256d30
Vortex-5 <vortex-5@users.noreply.github.com>
2015-09-04 17:56:54 -0400

    Added automatic cycle time adjustment based on if lighttpd is currently running.

--- bwmon/bwmon-running.sh
+++ bwmon/bwmon-running.sh
@@ -2,15 +2,33 @@
 SCRIPT_DIR=$(dirname ${0})
 SCRIPT_DIR=$(cd ${SCRIPT_DIR} && pwd)
 
+LIGHTTPD_RUNNING_CYCLE_TIME=300
+LIGHTTPD_NOT_RUNNING_CYCLE_TIME=10
+
+isLighttpdEnabled() {
+	psValue=$(ps | grep -c 'lighttpd -f')
+	if [ "$psValue" = "2" ]; then
+		echo 'true'
+	else
+		echo 'false'
+	fi
+}
+
+if [ "$(isLighttpdEnabled)" = "true" ]; then
+	CYCLE_TIME=$LIGHTTPD_RUNNING_CYCLE_TIME
+else
+	CYCLE_TIME=$LIGHTTPD_NOT_RUNNING_CYCLE_TIME
+fi
+
 # Startup confirmation
-echo "bandwidth monitor started use $SCRIPT_DIR/stop.sh to stop"
+echo "bandwidth monitor started with cycle time of $CYCLE_TIME use $SCRIPT_DIR/stop.sh to stop"
 
 # Bandwidth Download/Upload Rate Counter
 while :
 do
 	$SCRIPT_DIR/bwmon.sh setup
 	$SCRIPT_DIR/bwmon.sh read
-	sleep 10
+	sleep $CYCLE_TIME
 	$SCRIPT_DIR/bwmon.sh update /tmp/www/usage.js
 	$SCRIPT_DIR/bwmon.sh publish /tmp/www/usage.js /tmp/www/usage_stats.js
 done
