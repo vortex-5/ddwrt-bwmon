3d7a575800ed139e5a68e2f2de4a52993658e71d
Vortex-5 <vortex-5@users.noreply.github.com>
2020-08-08 16:44:02 -0400

    updated clean.sh for new service.

--- bwmon/clean.sh
+++ bwmon/clean.sh
@@ -2,18 +2,6 @@
 SCRIPT_DIR=$(dirname ${0})
 SCRIPT_DIR=$(cd ${SCRIPT_DIR} && pwd)
 
-psValue=$(ps | grep -c 'bwmon-running.sh')
-if [ "$psValue" = "2" ]; then
-	isRunning="true"
-	echo 'Monitor currently running'
-fi
-
-if [ "$isRunning" = "true" ]; then
-	$SCRIPT_DIR/stop.sh > /dev/null
-	echo 'Bandwidth monitoring stopped'
-	sleep 3
-fi
-
 # clean database
 echo 'Clearing bandwidth stats'
 rm "$SCRIPT_DIR/data/usage.js" 
@@ -21,9 +9,3 @@ rm /tmp/www/usage.js
 rm /tmp/www/usage_stats.js
 rm /tmp/traffic_*.tmp
 rm /tmp/sorted_*.tmp
-sleep 1
-
-if [ "$isRunning" = "true" ]; then
-	echo 'Restarting bandwidth monitoring'
-	$SCRIPT_DIR/start.sh > /dev/null
-fi
